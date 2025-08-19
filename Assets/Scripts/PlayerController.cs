f324a8a732ecf0eef17d2493298651b04e5b6120
limesoda314 <limesoda314@gmail.com>
2025-08-19 13:55:14 -0700

    Added a score and win text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic; 
 using UnityEngine;
-using UnityEngine.InputSystem; 
+using UnityEngine.InputSystem;
+using TMPro; 
 
 public class PlayerController : MonoBehaviour
 {
 
     //public variables
-    public float speed = 0; 
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject; 
 
     // private variables 
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    private int count; 
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,31 +25,60 @@ public class PlayerController : MonoBehaviour
     {
         
         rb = GetComponent<Rigidbody>();
-        Debug.Log("hello"); 
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false); 
+
+
+        //Debug.Log("game start"); 
     }
     public void OnMove(InputValue movementValue)
     {
-        Debug.Log("onMove"); 
+        //Debug.Log("onMove"); 
         Vector2 movementVector = movementValue.Get<Vector2>();
         
         movementX = movementVector.x;
         movementY = movementVector.y;
-        Debug.Log(movementX);
-        Debug.Log(movementY); 
+        //Debug.Log(movementX);
+        //Debug.Log(movementY); 
 
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+        if (count >= 45)
+        {
+            winTextObject.SetActive (true);
+        }
     }
 
     private void FixedUpdate()
     {
-        Debug.Log("FixedUpdate"); 
-        // Vector3 is a 3D vector or point (X, Z, Y) ??? that doesn't seem quite right
+        //Debug.Log("FixedUpdate"); 
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed); 
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        other.gameObject.SetActive(false);  
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+            count += 5;
+            SetCountText();
+        }
+
+        
+
     }
 
 
